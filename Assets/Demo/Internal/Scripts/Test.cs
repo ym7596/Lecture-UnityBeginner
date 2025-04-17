c34c2b5cf3a9ec41c5a5d78b382bf5370d6de905
DDUR <ym7596@naver.com>
2025-04-17 18:20:36 +0900

    feat : 프로퍼티

--- Assets/Demo/Internal/Scripts/Test.cs
+++ Assets/Demo/Internal/Scripts/Test.cs
@@ -7,10 +7,13 @@ public class Test : MonoBehaviour
     private Image image2;
     protected Image image3;
 
+    [SerializeField]
+    private int mynum;
+
     //변수 => 데이터를 저장하는 공간,
     // 숫자, 문자, 오브젝트 다양한 형태가능
 
-    int number = 0; //정수 -1,0,1,2,3,4,5,6
+   
     float nums = 0.1f; //소수점이 들어가는 숫자 float
     //double
     double num2 = 0;
@@ -18,10 +21,29 @@ public class Test : MonoBehaviour
     bool isOn = false; // false, true
     GameObject gObject;
 
+    private int number = 0; //정수 -1,0,1,2,3,4,5,6
+    public int myProperty
+    {
+        get
+        {
+            return number;
+        }
+        private set //세팅 결과값
+        {
+            isOn = true;
+            number = value +1; //35
+        }
+    }
+
+
+    public int callMyNum { get { return number; } }
+    public int callMyNum2 => number; //람다 함수
+
     //f(x) = y
     private void Start()
     {
         Attack(0,"abcd");
+        myProperty = 35;
     }
 
     int AttackPower(int power)
